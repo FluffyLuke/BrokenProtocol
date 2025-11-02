d874ba064bda5ec947898590db3fb112f1bcec9a
FluffyLuke <kacper.lukstaedt@gmail.com>
2025-11-02 15:04:44 +0100

    update: remove "no no" debug logs

--- Assets/Scripts/World/POC/Minecart/RailBlocker.cs
+++ Assets/Scripts/World/POC/Minecart/RailBlocker.cs
@@ -8,17 +8,13 @@ public class RailBlocker : MonoBehaviour, InteractableRequirement {
     [SerializeField] private Turnout turnout;
     public bool CheckRequirement() {
         foreach (var c in turnout.carts) {
-            Debug.LogWarning("Dupa");
             (bool _, SplineAnchor currentAnchor) = c.rail.GetCurrentAnchor(ref c.posData);
             foreach (var ca in turnout.railConnection.connectionAnchors) {
                 if (ca.anchor == currentAnchor) {
-                    Debug.LogWarning("Cycki");
                     return false;
                 }
             }
         }
-
-        Debug.LogWarning("A");
 
         return true;
     }
